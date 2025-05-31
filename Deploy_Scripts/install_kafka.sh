15dae3a4e60ad08d499fc648677d5adc10c12bee
mmp819 <mmp819@alumnos.unican.es>
2025-05-31 19:33:39 +0200

    Adjuntar instalación de Java y ajuste de puertos al instalar Kafka.

--- Deploy_Scripts/install_kafka.sh
+++ Deploy_Scripts/install_kafka.sh
@@ -13,6 +13,8 @@ install_kafka() {
 
     echo "[*] Descargando Kafka..."
     sshpass -p $PASS ssh -tt -p $PORT $USER@$node "wget https://dlcdn.apache.org/kafka/3.9.0/kafka_2.13-3.9.0.tgz -O /tmp/kafka.tgz"
+    echo $PASS | sshpass -p $PASS ssh -tt -p $PORT $USER@$node "sudo apt-get update"
+    echo $PASS | sshpass -p $PASS ssh -tt -p $PORT $USER@$node "sudo apt-get install -y default-jdk"
 
     echo "[*] Desempaquetando Kafka..."
     echo $PASS | sshpass -p $PASS ssh -tt -p $PORT $USER@$node "sudo tar -xzf /tmp/kafka.tgz -C /opt/kafka --strip-components=1"
@@ -21,6 +23,12 @@ install_kafka() {
     echo "[*] Añadiendo variables al sistema..."
     sshpass -p $PASS ssh -p $PORT $USER@$node "echo 'export KAFKA_HOME=/opt/kafka' >> ~/.bashrc"
     sshpass -p $PASS ssh -p $PORT $USER@$node "echo 'export PATH=\${PATH}:\${KAFKA_HOME}/bin' >> ~/.bashrc"
+
+    echo "[*] Añadiendo ajustes..."
+    sshpass -p $PASS ssh -p $PORT $USER@$KAFKA_NODE "
+    echo 'listeners=PLAINTEXT://0.0.0.0:9092' | sudo tee -a $KAFKA_DIR/config/server.properties
+    echo 'advertised.listeners=PLAINTEXT://10.0.3.10:9092' | sudo tee -a $KAFKA_DIR/config/server.properties
+"
 }
 
 install_kafka $1 # IP
